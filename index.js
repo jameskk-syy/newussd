10663d1029e7cebff3144f9771e886a42dadb51b
jameskk-syy <ronaldoeden70@gmail.com>
2024-08-27 09:04:20 +0300

    updated

--- index.js
+++ index.js
@@ -11,8 +11,43 @@ app.use(express.json());
 
 // Routes
 app.use("/home", home);
-app.get('/ussd',(req,res)=>{
- res.status(200).send(req.method)
+app.post('/ussd',(req,res)=>{
+// we get this from  users phone
+ const {
+    serviceCode,
+    sessionId,
+    phoneNumber,
+    text
+ } = req.body;
+
+ //create response 
+ let  response = "";
+
+ //first request 
+ if(text == ""){
+    response = `CON What do you  like to  check
+    1 Account Number
+    2 Account Information`;
+ }
+ else if(text == "1"){
+    // check  first level 
+    const accountNumber = "0796598108"
+    response = `END Your account number is ${accountNumber}`;
+ }
+ else if(text == "2"){
+    response = `CON What do  you like to  check  on  your account
+    1 Phone Number
+    2 Account Balance`;
+ }
+ else if(text == "2*1"){
+    response =`END Your phone number is ${phoneNumber}`
+ }
+ else if(text == "2*2"){
+    const accountBalance = "1000000"
+    response =`END Your account balance is ${accountBalance}`
+ }
+ res.set('content-type:text/plain');
+ res.send(response);
 });
 // connection
 const port = process.env.PORT || 9001;
