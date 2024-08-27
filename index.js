e1862faacd1a74ba557264513cf5c98411f3074e
jameskk-syy <ronaldoeden70@gmail.com>
2024-08-27 21:15:31 +0300

    updating

--- index.js
+++ index.js
@@ -1,76 +1,88 @@
 // Import packages
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser =  require('body-parser');
 const firebaseApp = require('./config');
-const { getFirestore, collection, addDoc } = require('firebase/firestore');
+const {getFirestore,collection,addDoc} = require('firebase/firestore');
 const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
 
-// establish firestore connection
-const fireStoreDb = getFirestore(firebaseApp);
+// establish  firestore  connection
+const fireStoreDb = getFirestore(app);
 
-// establish collection connection
-const collectionRef = collection(fireStoreDb, "USSD");
+//establish  collection connection
+
+const collectionRef = collection(fireStoreDb,"USSD");
 
 // Routes
 app.use("/home", home);
-app.post('/ussd', async (req, res) => {
-  // We get this from users phone
-  const { serviceCode, sessionId, phoneNumber, text } = req.body;
+app.post('/ussd',(req,res)=>{
+// we get this from  users phone
+ const {
+    serviceCode,
+    sessionId,
+    phoneNumber,
+    text
+ } = req.body;
 
-  // Create response
-  let response = "";
+ //create response 
+ let  response = "";
+ let data = "";
 
-  // First request
-  if (text == "") {
-    response = `CON What do you like to check:
+ //first request 
+ if(text == ""){
+    response = `CON What do you  like to  check
     1 Create Record
     2 Update Record
     3 Read Record
     4 Delete Account`;
-  } 
-  else if (text == "1") {
-    // Handle creating a record
-    const data = await createRecord(phoneNumber); // Pass phoneNumber
-    response = `END ${data}`;
-  } 
-  else if (text == "2") {
-    // Handle updating a record
-  } 
-  else if (text == "3") {
-    // Handle reading a record
-  } 
-  else if (text == "4") {
-    // Handle deleting a record
-  }
+ }
+ else if(text == "1"){
+   //check  first level 
+   response = 'END your data created';
+ }
+ else if(text == "2"){
+   response = 'END your data updated';
+ }
+ else if(text == "3"){
+   response = 'END your data retrieved';
+ }
+ else if(text == "4"){
+   response = 'END your data deleted';
 
-  res.set('content-type', 'text/plain');
-  res.send(response);
+ }
+ res.set('content-type:text/plain');
+ res.send(response);
 });
+// function createRecord(){
+//    //we will add our data here
+//    const data = {
+//       firstName: "James",
+//       lastName:"Maina",
+//       mobile:phoneNumber,
+//       amount:"0.00"
+//    }
+//    addDoc(collectionRef,data).then((result) =>
+//       {console.log(`Your record is saved successfully with ID ${result.id}`);
 
-async function createRecord(phoneNumber) {
-  // We will add our data here
-  const data = {
-    firstName: "James",
-    lastName: "Maina",
-    mobile: phoneNumber,
-    amount: "0.00"
-  };
-  
-  try {
-    const result = await addDoc(collectionRef, data);
-    console.log(`Your record is saved successfully with ID ${result.id}`);
-    return `Your record added successfully`;
-  } catch (err) {
-    console.error(`Error: ${err}`);
-    return `Failed to add your record`;
-  }
-}
+//       }).catch((err)=>{
+//          console.log(`Error ${err}`);
+//       })
+// return response = `END your record added successfully`;
+// }
+// function updateRecord(){
+//    response = `END Your record is updated successfully`;
+// }
+// function readRecord(){
+ 
+// }
+// function deleteRecord(){
+//    response = `END Your record is deleted successfully`;
+// }
 
 // connection
 const port = process.env.PORT || 9001;
